07a1670f991bfa24874ec14a6a98ad1794183e0a
Tomer <tomer.oz22@gmail.com>
2020-09-18 15:12:06 +0300

    Fixed routing to Index.html in profile presentations

--- profilePresntaion/views.py
+++ profilePresntaion/views.py
@@ -3,7 +3,7 @@ from .models import Profile
 
 # Create your views here.
 def index(request):
-    return render(request, 'profilePresntaion/index.html')
+    return render(request, 'profilePresntaion/Index.html')
 def present_profile(request, profile_pk):
     profile = get_object_or_404(Profile, pk=profile_pk)
     return render(request, 'profilePresntaion/profile.html', {'profile': profile})

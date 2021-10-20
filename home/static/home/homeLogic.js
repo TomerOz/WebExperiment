22bf9f4978688dcd0dd2e5d725b3b76c4848ed0a
TomerOz <tomer.oz@hotmail.com>
2021-10-20 15:03:34 +0300

    FML

--- home/static/home/homeLogic.js
+++ home/static/home/homeLogic.js
@@ -13,12 +13,12 @@ btn.onClick += function() {
 }
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() {
+span.onClick = function() {
   modal.style.display = "none";
 }
 
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
+window.oClick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
   }
